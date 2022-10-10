85487f78cac12df080895c6b3205a0bc9bf9dd04
dhiksha1234 <dhiksha.sivaraman@crystaldelta.com>
2022-10-10 13:03:15 +0530

    test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
  import Home from './first';
 
-//  const App =() =>{
-//     return (
-//         <h1>Hi</h1>
-//     )
-// }
+ 
 
 ReactDOM.render(<Home/>,document.getElementById("root"))
